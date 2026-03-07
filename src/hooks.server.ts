ac0c8f595e9263477f2c8c01db5928a479c7108b
Andreas Skepp <andreasskepp@gmail.com>
2026-03-07 17:34:13 +0100

    mobilfix layout

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -3,6 +3,7 @@ import { SUPABASE_URL, SUPABASE_ANON_KEY } from '$env/static/private';
 import type { Handle } from '@sveltejs/kit';
 
 export const handle: Handle = async ({ event, resolve }) => {
+    // Skapa EN global Supabase-klient
     const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
         auth: {
             persistSession: false
@@ -11,6 +12,7 @@ export const handle: Handle = async ({ event, resolve }) => {
 
     event.locals.supabase = supabase;
 
+    // Läs token från cookies
     const access_token = event.cookies.get('sb-access-token');
 
     if (access_token) {
@@ -20,5 +22,6 @@ export const handle: Handle = async ({ event, resolve }) => {
         event.locals.user = null;
     }
 
+    // Kör vidare till route/layout
     return resolve(event);
 };
