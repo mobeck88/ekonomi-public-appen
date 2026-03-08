8f6a17705f99a56145c91dab2bb0f66e0e196ff9
Andreas Skepp <andreasskepp@gmail.com>
2026-03-08 17:52:40 +0100

    Revert hooks to previous working version

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,18 +1,24 @@
-import { createSupabaseServerClient } from '@supabase/auth-helpers-sveltekit';
+import { createClient } from '@supabase/supabase-js';
 import { SUPABASE_URL, SUPABASE_ANON_KEY } from '$env/static/private';
+import type { Handle } from '@sveltejs/kit';
 
-export const handle = async ({ event, resolve }) => {
-    event.locals.supabase = createSupabaseServerClient({
-        supabaseUrl: SUPABASE_URL,
-        supabaseKey: SUPABASE_ANON_KEY,
-        event
+export const handle: Handle = async ({ event, resolve }) => {
+    const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
+        auth: {
+            persistSession: false
+        }
     });
 
-    const {
-        data: { session }
-    } = await event.locals.supabase.auth.getSession();
+    event.locals.supabase = supabase;
 
-    event.locals.user = session?.user ?? null;
+    const access_token = event.cookies.get('sb-access-token');
+
+    if (access_token) {
+        const { data } = await supabase.auth.getUser(access_token);
+        event.locals.user = data.user ?? null;
+    } else {
+        event.locals.user = null;
+    }
 
     return resolve(event);
 };
