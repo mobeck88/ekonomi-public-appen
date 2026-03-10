d65b59fd629bf67224d6db1f764732b589d693d6
Andreas Skepp <andreasskepp@gmail.com>
2026-03-10 12:22:06 +0100

    update

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,12 +1,4 @@
-import { redirect } from '@sveltejs/kit';
-
-export const load = ({ locals, url }) => {
-    const publicRoutes = ['/login', '/register'];
-
-    if (!locals.user && !publicRoutes.includes(url.pathname)) {
-        throw redirect(303, '/login');
-    }
-
+export const load = ({ locals }) => {
     return {
         user: locals.user,
         householdId: locals.householdId
