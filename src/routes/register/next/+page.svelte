d7de4e90938e45178470f63198d68e24618e7d38
Andreas Skepp <andreasskepp@gmail.com>
2026-03-17 12:28:03 +0100

    update

--- src/routes/register/next/+page.svelte
+++ src/routes/register/next/+page.svelte
@@ -8,7 +8,7 @@
 <div class="section">
     <h2>Gå med i hushåll (rekommenderas)</h2>
 
-    <form method="POST" action="join" class="form">
+    <form method="POST" action="?/join" class="form">
         <label for="code">Hushållskod</label>
         <input
             id="code"
@@ -29,7 +29,7 @@
 <div class="section" style="margin-top: 1.5rem;">
     <h2>Skapa nytt hushåll</h2>
 
-    <form method="POST" action="create">
+    <form method="POST" action="?/create">
         <button type="submit">Skapa nytt hushåll</button>
     </form>
 </div>
