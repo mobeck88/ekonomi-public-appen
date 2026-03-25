496279ed5a991a7b4b4d650082c56a76ca6662b0
Andreas Skepp <andreasskepp@gmail.com>
2026-03-25 16:37:52 +0100

    update

--- src/routes/keepalive/+server.ts
+++ src/routes/keepalive/+server.ts
@@ -2,12 +2,10 @@ import { json } from '@sveltejs/kit';
 import { SUPABASE_ANON_KEY } from '$env/static/private';
 
 export async function GET() {
-    // Gör en request som Supabase ALLTID loggar
     await fetch('https://stosexhxuaazqgtohopx.supabase.co/rest/v1/any_table', {
         method: 'GET',
         headers: {
             apikey: SUPABASE_ANON_KEY,
-            // Gör så att Supabase svarar 401 istället för 404
             Authorization: 'Bearer invalid'
         }
     });
