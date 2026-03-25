0798a413ca5881373f0f5b86c7caa3da001543af
Andreas Skepp <andreasskepp@gmail.com>
2026-03-25 14:35:38 +0100

    update

--- supabase/functions/keepalive/index.ts
+++ supabase/functions/keepalive/index.ts
@@ -1,10 +1,7 @@
 export const runtime = "edge";
 
-export async function GET() {
-  return new Response("ok", {
-    status: 200,
-    headers: {
-      "content-type": "text/plain"
-    }
+export async function HEAD() {
+  return new Response(null, {
+    status: 200
   });
 }
