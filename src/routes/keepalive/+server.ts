9d6a966efdee00193d3fb0cc5175782f6afbff84
Andreas Skepp <andreasskepp@gmail.com>
2026-03-25 15:49:47 +0100

    update

--- src/routes/keepalive/+server.ts
+++ src/routes/keepalive/+server.ts
@@ -1,9 +1,16 @@
 import { json } from '@sveltejs/kit';
+import { SUPABASE_ANON_KEY } from '$env/static/private';
 
 export async function GET() {
-  // Gör en enkel request mot Supabase REST API
-  // Den kommer ge 401, men databasen vaknar och loggar det.
-  await fetch('https://stosexhxuaazqgtohopx.supabase.co/rest/v1/any_table');
+    // Gör en request som Supabase ALLTID loggar
+    await fetch('https://stosexhxuaazqgtohopx.supabase.co/rest/v1/any_table', {
+        method: 'GET',
+        headers: {
+            apikey: SUPABASE_ANON_KEY,
+            // Gör så att Supabase svarar 401 istället för 404
+            Authorization: 'Bearer invalid'
+        }
+    });
 
-  return json({ status: 'ok' });
+    return json({ status: 'ok' });
 }
