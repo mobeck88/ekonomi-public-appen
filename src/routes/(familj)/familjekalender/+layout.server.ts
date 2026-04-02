a174eca900e210859a0c0e547ef386331707cab3
Andreas Skepp <andreasskepp@gmail.com>
2026-04-02 13:04:52 +0200

    update

--- src/routes/(familj)/familjekalender/+layout.server.ts
+++ src/routes/(familj)/familjekalender/+layout.server.ts
@@ -1,13 +1,16 @@
 import { redirect } from '@sveltejs/kit';
 import type { LayoutServerLoad } from './$types';
 
-export const load: LayoutServerLoad = async ({ locals }) => {
-    const user = locals.user;
-    const householdId = locals.householdId;
+export const load: LayoutServerLoad = async ({ locals, parent }) => {
+    const parentData = await parent();
+
+    const user = parentData.user;
+    const householdId = parentData.householdId;
 
     if (!user) throw redirect(303, '/login');
 
     return {
+        ...parentData, // ⭐ Behåll ALLT från globala layouten
         user,
         householdId
     };
