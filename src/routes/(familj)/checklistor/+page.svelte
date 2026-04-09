49e814a2a53923768211bc32398602afd9d626d8
Andreas Skepp <andreasskepp@gmail.com>
2026-04-09 15:57:44 +0200

    update

--- src/routes/(familj)/checklistor/+page.svelte
+++ src/routes/(familj)/checklistor/+page.svelte
@@ -4,14 +4,22 @@
 
 <h1>Checklistor</h1>
 
-<ul class="items">
-    {#each data.checklists as checklist}
-        <li>
-            <a href={`/checklistor/${checklist.id}`}>
-                {checklist.title}
-            </a>
-        </li>
-    {/each}
-</ul>
+<div class="section">
+    <div class="section-header">Alla checklistor</div>
+
+    {#if data.checklists.length === 0}
+        <div class="empty">Inga checklistor ännu.</div>
+    {:else}
+        <ul style="list-style: none; padding: 1rem; margin: 0;">
+            {#each data.checklists as checklist}
+                <li style="padding: 0.5rem 0;">
+                    <a href={`/checklistor/${checklist.id}`} style="font-size: 1rem; color: #2563eb;">
+                        {checklist.title}
+                    </a>
+                </li>
+            {/each}
+        </ul>
+    {/if}
+</div>
 
 <style src="./checklistor.css"></style>
